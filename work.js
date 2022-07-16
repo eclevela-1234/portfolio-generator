4c9f04ef666601adab89f8641585afacbe090904
Eliot Cleveland <eclevela@richmond.edu>
2022-07-15 22:28:54 -0400

    added work script for executing lesson examples

---
+++ work.js
@@ -0,0 +1,12 @@
+const message = 'Hello Node!';
+
+if (true === true) {
+  const message = 'Hello ES6!';
+  let sum = 5;
+  sum += 10;
+  console.log(message);
+  console.log(sum);
+}
+
+console.log(message);
+console.log(sum);
