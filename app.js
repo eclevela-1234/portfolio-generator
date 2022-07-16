c50e0b9e5200e948953acb2e1351b432af9861cc
Eliot Cleveland <eclevela@richmond.edu>
2022-07-15 22:23:39 -0400

    saving progress in lesson

---
+++ app.js
@@ -0,0 +1,23 @@
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+    name: 'Eliot',
+    age: 36
+};
+
+personObj.age = 37;
+personObj.occupation = "Developer"
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    for (let i=0; i <profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+};
+
+printProfileData(profileDataArgs);
+
