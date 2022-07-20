c46eac9dfd46bae73cf51cad57ac78a0d0a9a15a
Eliot Cleveland <eclevela@richmond.edu>
2022-07-19 23:30:08 -0400

    refactor template literal

--- src/page-template.js
+++ src/page-template.js
@@ -1,8 +1,8 @@
 // create the about section
 
-const generateAbout = aboutText => {
+const generateAbout = (aboutText) => {
   if (!aboutText) {
-    return '';
+    return "";
   }
   return `
   <section class="my-3" id="about">
@@ -13,8 +13,31 @@ const generateAbout = aboutText => {
 };
 
 const generateProjects = projectsArr => {
-  const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
-      return `
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
@@ -25,20 +48,15 @@ const generateProjects = projectsArr => {
             <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
           </div>
         `;
-    });
-
-  return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${projectHtmlArr.join('')}
+        })
+        .join('')}
       </div>
     </section>
   `;
 };
-module.exports = templateData => {
-  const { projects, about, ...header } = templateData; 
-    return `
+module.exports = (templateData) => {
+  const { projects, about, ...header } = templateData;
+  return `
     <!DOCTYPE html> 
     <html lang="en"> 
     <head>
@@ -54,7 +72,9 @@ module.exports = templateData => {
     <body>
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${
+          header.name
+        }</h1>
         <nav class="flex-row">
           <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
             header.github
@@ -67,10 +87,11 @@ module.exports = templateData => {
       ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
+    header.name
+  }</h3>
     </footer>
   </body>
   </html>
     `;
-  };
-
+};
